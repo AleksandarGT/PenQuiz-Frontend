afa9d8b56f4289ee63095f892a9ef33d96e6d723
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2021-12-31 12:29:13 +0200

    Added more basic tests

--- cypress/integration/web_tests/test_active_page.js
+++ cypress/integration/web_tests/test_active_page.js
@@ -1,9 +1,29 @@
 describe('Initial page load', () => {
-    it('Check existing elements', () => {
-      cy.visit('http://localhost:19006/Login')
-      cy.contains('ConQuiz')
-      cy.contains('start your adventure now')
+  it('Check existing elements', () => {
+    cy.visit('https://conquiz.netlify.app/Login')
 
-      cy.contains('Sign in with Google').click()
-    })
+    cy.contains('ConQuiz')
+    cy.contains('start your adventure now')
+
+    cy.contains('Sign in with Google').click()
   })
+
+  it('Check rules modal', () => {
+    cy.visit('https://conquiz.netlify.app/Login')
+
+    cy.contains('ConQuiz')
+    cy.contains('start your adventure now')
+
+    cy.contains('Rules').click()
+    cy.contains('ConQuiz is a round-based PvP trivia game revolved around capturing the territories of the continent of Antarctica.')
+  })
+
+  it('Check about button', () => {
+    cy.visit('https://conquiz.netlify.app/Login')
+
+    cy.contains('ConQuiz')
+    cy.contains('start your adventure now')
+
+    cy.contains('About').click()
+  })
+})
