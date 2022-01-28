86ce766f2c999ed5ffd629d6d1ffb202d51fb0d5
BoostedPenguin <alex20009@abv.bg>
2022-01-28 13:44:16 +0100

    Added unverified fetching from client

--- components/VerifyQuestionComponents/VerifyQuestionComponent.js
+++ components/VerifyQuestionComponents/VerifyQuestionComponent.js
@@ -1,16 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import { View, ImageBackground, StyleSheet, ActivityIndicator, Platform } from 'react-native'
-import { Text, Button, Center, Box, Pressable, Input, Alert, VStack, HStack, IconButton, CloseIcon, AspectRatio } from 'native-base'
+import { Text, Button, Center, Box, Pressable, Input, Alert, VStack, HStack, IconButton, CloseIcon, AspectRatio, ScrollView } from 'native-base'
 import { useRecoilValue } from 'recoil'
 import { authAtom } from '../../state'
 import { useFetchWrapper } from '../../helpers'
-import { BACKEND_GAME_API_URL } from '@env'
+import { BACKEND_QUESTION_API_URL } from '@env'
 import { useIsFocused } from '@react-navigation/native'
 import { MaterialIcons } from '@expo/vector-icons';
 import { Dimensions } from 'react-native';
 
 export function VerifyQuestionComponent() {
     const windowWidth = Dimensions.get('screen').width;
+    const [questionsResponse, setQuestionsResponse] = useState()
+
+    const fetchWrapper = useFetchWrapper()
+    const isFocused = useIsFocused()
+
+    useEffect(() => {
+        if (!isFocused) return
+        fetchQuestions(1)
+
+    }, [isFocused])
+
+    function fetchQuestions(pageIndex) {
+        const baseUrl = `${BACKEND_QUESTION_API_URL}/api/questionadmin`
+        fetchWrapper.get(`${baseUrl}/${pageIndex}`)
+            .then(response => {
+                setQuestionsResponse(response)
+            })
+            .catch(er => {
+                console.log(er)
+            })
+    }
+
+
+    const goPrevious = () => {
+        if (!questionsResponse) return;
+        fetchQuestions(questionsResponse.pageIndex - 1)
+    }
+
+    const goNext = () => {
+        if (!questionsResponse) return;
+        fetchQuestions(questionsResponse.pageIndex + 1)
+    }
+
 
     function ButtonTemplate({ onPressEvent, buttonText, left, right }) {
         return (
@@ -26,7 +59,7 @@ export function VerifyQuestionComponent() {
                                         <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
                                     </Center>}
 
-                                    <Text fontSize={{ base: "md", md: "md", lg: "lg", xl: "xl" }} color="#032157" fontStyle="italic">
+                                    <Text fontSize={{ base: "sm", md: "md", lg: "lg", xl: "xl" }} color="#032157" fontStyle="italic">
                                         {buttonText}
                                     </Text>
 
@@ -48,7 +81,7 @@ export function VerifyQuestionComponent() {
             <Pressable>
                 {({ isHovered, isFocused, isPressed }) => {
                     return (
-                        <Box width="100%" p={2} borderWidth={2} borderColor="#B6F6FF" bg={isPressed ? "#000" : isHovered ? "#123" : "#0F5688"}>
+                        <Box width="100%" p={2} borderWidth={2} borderColor="#B6F6FF" bg={isPressed ? "#0C3958" : isHovered ? "#0E466D" : "#0F5688"}>
                             <HStack justifyContent="space-between">
                                 <Box>
                                     <HStack>
@@ -60,7 +93,7 @@ export function VerifyQuestionComponent() {
                                             </Center>
                                         </AspectRatio>
                                         <Text alignSelf="center" ml={3} isTruncated maxW={windowWidth * 0.4} fontSize={{ base: "md", md: "md", lg: "lg", xl: "xl" }}>
-                                        {question}
+                                            {question}
                                         </Text>
                                     </HStack>
                                 </Box>
@@ -79,27 +112,36 @@ export function VerifyQuestionComponent() {
 
     return (
         <ImageBackground source={Platform.OS === 'web' ? require('../../assets/homeBackground.svg') : require('../../assets/homeBackground.png')} resizeMode="cover" style={styles.image}>
-            <View style={{ alignItems: "center" }}>
+            <View style={{ alignItems: "center", flex: 0.8 }}>
 
-                <Box width="90%" minWidth="70%" bg="#071D56" p={10} borderRadius={25}>
+                <Box width="90%" flex={1} minWidth="70%" bg="#071D56" p={8} borderRadius={25}>
                     <Text mb={5} textAlign="center" color="#fff" fontSize={{ base: "md", md: "lg", lg: "xl", xl: "3xl" }} style={{ fontFamily: 'Before-Collapse', }}>
                         User question submissions
                     </Text>
 
-                    <Box>
-                        <VStack>
-                            {[...Array(6)].map((e, index) => {
+                    <VStack style={{ flex: 1 }}>
+                        <ScrollView flex={1}>
+                            {questionsResponse && questionsResponse.questions.map(e => {
                                 return (
-                                    <QuestionRow type={index % 2 == 0 ? "multiple" : "number"} question={Math.random().toString() + Math.random().toString() + Math.random().toString() + Math.random().toString() + Math.random().toString() + Math.random().toString()} />
+                                    <QuestionRow key={e.id} type={e.type} question={e.question} />
                                 )
                             })}
-                        </VStack>
-                    </Box>
-                    <HStack justifyContent="space-between" mt={5} mx={2}>
-                        <ButtonTemplate left buttonText="Previous" />
-                        <ButtonTemplate right buttonText="Next" />
+                            {questionsResponse && questionsResponse.questions.length == 0 &&
+                                <Text textAlign="center">No questions that require verification at this moment.{"\n"}Check back later.</Text>
+                            }
+                        </ScrollView>
 
-                    </HStack>
+                        {!questionsResponse && <ActivityIndicator size="large" />}
+                    </VStack>
+
+                    {questionsResponse && <HStack justifyContent={questionsResponse.hasPreviousPage && questionsResponse.hasNextPage ? "space-between" : questionsResponse.hasNextPage ? "flex-end" : "flex-start"} mt={5} mx={2}>
+                        {questionsResponse.hasPreviousPage && <ButtonTemplate left buttonText="Previous" onPressEvent={() => {
+                            goPrevious()
+                        }} />}
+                        {questionsResponse.hasNextPage && <ButtonTemplate right buttonText="Next" onPressEvent={() => {
+                            goNext()
+                        }} />}
+                    </HStack>}
 
                 </Box>
             </View>
