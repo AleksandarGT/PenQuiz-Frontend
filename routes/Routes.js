13ce2d01bbfa95af46d1c53aa1fe174bb6ad4fcc
BoostedPenguin <alex20009@abv.bg>
2022-05-01 18:40:43 +0200

    Debug mode addition

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Platform, Text, StyleSheet, View, ActivityIndicator, Button, Image } from 'react-native'
-import { useRecoilValue } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { useAuthActions } from '../hooks'
 import LoginComponent from '../components/LoginComponent'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -8,7 +8,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { navigationRef } from '../helpers'
 import TestingSvg from '../components/Testing/TestingSvg'
 import * as Linking from 'expo-linking'
-import { authStatus, gameInstanceAtom } from '../state'
+import { authAtom, authStatus, gameInstanceAtom } from '../state'
 import { HomeDrawer } from './HomeDrawer'
 import { LoadingComponent } from './LoadingComponent'
 import { GameLobby } from '../components/GameLobby'
@@ -16,21 +16,30 @@ import GameMap from '../components/GameMapComponents/GameMap'
 import MultipleChoiceScreen from '../components/GameMapComponents/MultipleChoiceScreen'
 import NumberChoiceScreen from '../components/GameMapComponents/NumberChoiceScreen'
 import { GetGameState } from '../components/GameMapComponents/CommonGameFunc'
+import CharacterCard from '../components/CharacterComponents/CharacterCard'
+import BaseCharacterComponent from '../components/CharacterComponents/BaseCharacterComponent'
 
 export * from './LoadingComponent'
 
 
 const Stack = createStackNavigator()
 const prefix = Linking.createURL('http://localhost:19006')
+export const isDebugRunning = true
+
 export function Routes() {
   const localAuthStatus = useRecoilValue(authStatus)
   const game = useRecoilValue(gameInstanceAtom)
 
+  const setUser = useSetRecoilState(authAtom)
   const useuseAuthActions = useAuthActions()
   useEffect(() => {
     // Call refresh token. If auth is successful you will navigate to main component
     // If not to login / register component
-    useuseAuthActions.refreshToken()
+    !isDebugRunning && useuseAuthActions.refreshToken()
+
+    isDebugRunning && setUser({
+      id: 5
+    })
   }, [])
 
   const linking = {
@@ -67,6 +76,12 @@ export function Routes() {
   }, [game?.gameState])
 
   function SwitchAuthState() {
+
+    // return (
+    //   <Stack.Screen name="Loading" options={{ headerShown: false }} >
+    //     {() => <BaseCharacterComponent />}
+    //   </Stack.Screen>
+    // )
 
     if (localAuthStatus === 'LOADING') {
       return (
