504858717c0865d715dfc4147f895006f5d92740
BoostedPenguin <alex20009@abv.bg>
2022-06-25 12:10:37 +0200

    Added viking button component

--- components/GameMapComponents/CharacterComponents/VikingActionComponent.tsx
+++ components/GameMapComponents/CharacterComponents/VikingActionComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Pressable, Text } from "native-base";
+import { Box, Center, Divider, HStack, Image, Pressable, Text, Tooltip, View } from "native-base";
 import React, { useEffect, useMemo, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { VikingUseAbility, WizardUseMultipleChoiceHint } from "../../../hooks";
@@ -74,28 +74,53 @@ export default function VikingActionComponent({ question, invisible }
 
 
     return (
-        <Pressable opacity={invisible ? 0 : 100} disabled={invisible || areAllFortifiesUsed || vikingAbilityUsed} onPress={() => {
-            VikingUseAbility(globalDisplayTime)
-            setVikingAbilityUsed(true)
-        }}>
-            {({ isHovered, isFocused, isPressed }) => {
-                return (
-                    <Box style={{
-                        aspectRatio: 1 / 1,
-                    }} p={2} mt={6} shadow={3} bg={
-                        isPressed ? "#0D569B" : isHovered ? "#06326F" : "#071D56"
-                    } borderRadius={10}>
-                        <Center>
-                            <Text selectable={false} >
-                                Viking
-                            </Text>
-                            <Text selectable={false}>
-                                {getThisVikingAbilities?.fortifyCapitalUseCount} / {getThisVikingAbilities?.fortifyCapitalMaxUseCount}
-                            </Text>
-                        </Center>
-                    </Box>
-                )
-            }}
-        </Pressable>
+        <View opacity={invisible ? 0 : 100} >
+            <HStack mt={6}>
+                <Center>
+                    <Pressable disabled={invisible || areAllFortifiesUsed || vikingAbilityUsed} onPress={() => {
+                        VikingUseAbility(globalDisplayTime)
+                        setVikingAbilityUsed(true)
+                    }}>
+                        {({ isHovered, isFocused, isPressed }) => {
+                            return (
+                                <Box justifyContent={"center"} style={{
+                                    aspectRatio: 1 / 1,
+                                }} px={1.5} shadow={3} bg={
+                                    isPressed ? "#3DAE15" : isHovered ? "#31930E" : "#216609"
+                                } borderRadius={25}>
+                                    <Center >
+                                        <HStack alignItems={"center"}>
+
+                                            <Text fontWeight={"bold"} color={"#C5DFFF"} fontSize={"lg"} selectable={false} >
+                                                Help!
+                                            </Text>
+                                            <Image
+                                                source={require("../../../assets/characterAssets/vikingAxe.svg")}
+                                                alt="Alternate Text"
+                                                resizeMode="contain"
+                                                size="xs"
+                                            />
+                                        </HStack>
+                                        <Divider my={1} backgroundColor={"#C5DFFF"} />
+                                        <Text fontWeight={"bold"} fontSize={"md"} color={"#C5DFFF"} selectable={false}>
+                                            {getThisVikingAbilities?.fortifyCapitalUseCount} / {getThisVikingAbilities?.fortifyCapitalMaxUseCount} uses
+                                        </Text>
+                                    </Center>
+                                </Box>
+                            )
+                        }}
+                    </Pressable>
+                </Center>
+
+                <Tooltip label="Adds an additional capital round" placement="right">
+                    <Image
+                        source={require("../../../assets/characterAssets/hintPopup.svg")}
+                        alt="Alternate Text"
+                        resizeMode="contain"
+                        size="xs"
+                    />
+                </Tooltip>
+            </HStack>
+        </View>
     )
 }
