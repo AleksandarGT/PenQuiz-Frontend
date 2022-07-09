e829a02790ec5ab6308f3b83589d0b80b65bb87f
BoostedPenguin <alex20009@abv.bg>
2022-07-09 11:12:18 +0200

    Updated download apk

--- App.tsx
+++ App.tsx
@@ -106,7 +106,7 @@ export default function App() {
 
         {servicesStatus.Status == Status.INACTIVE || !fontsLoaded ?
           <LoadingComponent message={servicesStatus.Message} /> : isMobileFromWeb ?
-            <LoadingComponent message='Please download and install PenQuiz app for Android devices:' link='https://onedrive.live.com/download?cid=D25E3AABFBD442D5&resid=D25E3AABFBD442D5%2111938&authkey=AJEh8Ch5F2nIbHs' /> :
+            <LoadingComponent message='Please download and install PenQuiz app for Android devices:' link='https://onedrive.live.com/download?cid=D25E3AABFBD442D5&resid=d25e3aabfbd442d5%2112006&authkey=AFUTx5Dgm91DciU' /> :
             <Routes />
         }
 
